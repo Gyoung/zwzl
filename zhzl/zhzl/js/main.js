05409bedc2525ce443000ec45f31fa197ee24066
gyoung <652105072@qq.com>
2017-11-25 16:58:02 +0800

    增加QQ聊天

--- zhzl/zhzl/js/main.js
+++ zhzl/zhzl/js/main.js
@@ -16,16 +16,18 @@ $(document).ready(function(){
 
      
     //animated header class
-    $(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-     //console.log(scroll);
-    if (scroll > 200) {
-        //console.log('a');
-        $(".navigation").addClass("animated");
-    } else {
-        //console.log('a');
-        $(".navigation").removeClass("animated");
-    }});
+    //$(window).scroll(function() {    
+    //var scroll = $(window).scrollTop();
+    // //console.log(scroll);
+    //if (scroll > 200) {
+    //    //console.log('a');
+    //    $(".navigation").addClass("animated");
+    //} else {
+    //    //console.log('a');
+    //    $(".navigation").removeClass("animated");
+    //}
+    //}
+    //);
 
     //$('#countdown_dashboard').countDown({
     //    targetDate: {
