658aa4cabe3d88f60cff9c9d3e8e24264c3e8e2e
gyoung <652105072@qq.com>
2018-09-09 23:56:07 +0800

    增加udp server

---
+++ zhzl/zhzl/Global.asax.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace zhzl
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        LeafUDPClient udpserver = new LeafUDPClient();
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            try
+            {
+                IPEndPoint ipLocalEndPoint= new IPEndPoint(IPAddress.Any, 6000);
+               
+               
+                udpserver.NetWork = new UdpClient(ipLocalEndPoint);
+                udpserver.ipLocalEndPoint = ipLocalEndPoint;
+                udpserver.NetWork.BeginReceive(new AsyncCallback(ReceiveCallback), udpserver);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// 接收到数据
+        /// </summary>
+        /// <param name="ar"></param>
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            LeafUDPClient userver = (LeafUDPClient)ar.AsyncState;
+            string ConnName = "";
+            try
+            {
+                if (userver.NetWork.Client != null)
+                {
+                    IPEndPoint fclient = userver.ipLocalEndPoint;
+                    Byte[] recdata = userver.NetWork.EndReceive(ar, ref fclient);
+                    ConnName = userver.ipLocalEndPoint.Port + "->" + fclient.ToString();
+                    string data= new ASCIIEncoding().GetString(recdata);
+                   
+                  
+                }
+            }
+            catch (ObjectDisposedException ex) { }
+            catch (Exception ex)
+            {
+               
+            }
+            finally
+            {
+                if (userver.NetWork.Client != null)
+                {
+                    userver.NetWork.BeginReceive(new AsyncCallback(ReceiveCallback), userver);//继续异步接收数据
+                }
+            }
+        }
+
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
